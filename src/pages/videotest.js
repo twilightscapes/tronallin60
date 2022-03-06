11decb52fa33cb2ecb6ca55e9a8d79cdb1ccf24f
twilightscapes <ihatespam@gmail.com>
2022-03-05 22:18:52 -0600

    videotest

--- src/pages/videotest.js
+++ src/pages/videotest.js
@@ -1,52 +1,24 @@
-import React, { useState, useRef } from "react";
-import { Layout } from "../../components/layout";
+import React, { useState, useRef, useEffect } from "react";
+import { findDOMNode } from "react-dom";
 
-import { Link } from "gatsby"
-// import { graphql } from "gatsby"
-// import { GatsbyImage } from 'gatsby-plugin-image'
-// import TwilightLogo from "../../static/assets/logo.svg"
-// import { StaticImage } from "gatsby-plugin-image"
-import { useSiteMetadata } from "../../hooks/use-site-metadata"
-// import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox"
-import ReactPlayer from 'react-player/lazy'
-import Controls from "../../components/Controls"
+// import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import ReactPlayer from "react-player";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
+
 import Slider from "@material-ui/core/Slider";
 import Tooltip from "@material-ui/core/Tooltip";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import VolumeUp from "@material-ui/icons/VolumeUp";
+import VolumeDown from "@material-ui/icons/VolumeDown";
+import VolumeMute from "@material-ui/icons/VolumeOff";
+import FullScreen from "@material-ui/icons/Fullscreen";
+import Popover from "@material-ui/core/Popover";
 import screenful from "screenfull";
-
-// import Controls from "../../components/VideoControls"
-// import LightCycle from "../../static/assets/light-cycle.svg"
-// import { ImPlay } from "react-icons/im"
-// import LightCycle from "../../static/assets/light-cycle.svg"
-// import QuoraStrike from "../../static/assets/quora-strike.svg"
-// import LoveSignal from "../../static/assets/love-signal.svg"
-// import Castor from "../../static/assets/castor.svg"
-// import SamFly from "../../static/assets/sam-fly.svg"
-// import SamFlynn from "../../static/assets/sam-flynn.png"
-// import CluGuy from "../../static/assets/clu.png"
-// import TronGuy from "../../static/assets/tron.png"
-// import MCP from "../../static/assets/mcp.svg"
-// import McpEffects from "../../static/assets/mcp-effects.svg"
-// import QuoraStand from "../../static/assets/quora-ass.png"
-// import KevinFlynn from "../../static/assets/kevin-flynn.png"
-// import TronChick from "../../static/assets/tron-chick1.svg"
-import { ImPlay } from "react-icons/im"
-import SorryFolks from "../../../static/assets/sorry-folks-moose.svg"
-import { RiArrowLeftSLine, RiBugLine } from "react-icons/ri"
-import styled from "styled-components"
-const CustomBox = styled.div`
-
-
-
-@media (min-width: 58em) {
-
-}
-
-
-`
-
-
+import Controls from "../components/VideoControls";
 
 const useStyles = makeStyles((theme) => ({
   playerWrapper: {
@@ -179,9 +151,7 @@ const format = (seconds) => {
 
 let count = 0;
 
-function Sirens() {
-
-
+function VideoTest() {
   const classes = useStyles();
   const [showControls, setShowControls] = useState(false);
   // const [count, setCount] = useState(0);
@@ -191,7 +161,7 @@ function Sirens() {
   const [state, setState] = useState({
     pip: false,
     playing: true,
-    controls: true,
+    controls: false,
     light: false,
 
     muted: false,
@@ -206,7 +176,7 @@ function Sirens() {
   const playerRef = useRef(null);
   const playerContainerRef = useRef(null);
   const controlsRef = useRef(null);
-  const canvasRef = useRef(null);
+  // const canvasRef = useRef(null);
   const {
     playing,
     controls,
@@ -235,7 +205,7 @@ function Sirens() {
 
   const handleProgress = (changeState) => {
     if (count > 3) {
-      controlsRef.current.style.visibility = "hidden";
+      controlsRef.current.style.visibility = "visible";
       count = 0;
     }
     if (controlsRef.current.style.visibility == "visible") {
@@ -289,7 +259,7 @@ function Sirens() {
   };
 
   const hanldeMouseLeave = () => {
-    controlsRef.current.style.visibility = "hidden";
+    controlsRef.current.style.visibility = "visible";
     count = 0;
   };
 
@@ -307,30 +277,30 @@ function Sirens() {
     setState({ ...state, muted: !state.muted });
   };
 
-  const addBookmark = () => {
-    const canvas = canvasRef.current;
-    canvas.width = 160;
-    canvas.height = 90;
-    const ctx = canvas.getContext("2d");
+  // const addBookmark = () => {
+  //   const canvas = canvasRef.current;
+  //   canvas.width = 160;
+  //   canvas.height = 90;
+  //   const ctx = canvas.getContext("2d");
 
-    ctx.drawImage(
-      playerRef.current.getInternalPlayer(),
-      0,
-      0,
-      canvas.width,
-      canvas.height
-    );
-    const dataUri = canvas.toDataURL();
-    canvas.width = 0;
-    canvas.height = 0;
-    const bookmarksCopy = [...bookmarks];
-    bookmarksCopy.push({
-      time: playerRef.current.getCurrentTime(),
-      display: format(playerRef.current.getCurrentTime()),
-      image: dataUri,
-    });
-    setBookmarks(bookmarksCopy);
-  };
+  //   ctx.drawImage(
+  //     playerRef.current.getInternalPlayer(),
+  //     0,
+  //     0,
+  //     canvas.width,
+  //     canvas.height
+  //   );
+  //   const dataUri = canvas.toDataURL();
+  //   canvas.width = 0;
+  //   canvas.height = 0;
+  //   const bookmarksCopy = [...bookmarks];
+  //   bookmarksCopy.push({
+  //     time: playerRef.current.getCurrentTime(),
+  //     display: format(playerRef.current.getCurrentTime()),
+  //     image: dataUri,
+  //   });
+  //   setBookmarks(bookmarksCopy);
+  // };
 
   const currentTime =
     playerRef && playerRef.current
@@ -346,58 +316,23 @@ function Sirens() {
 
   const totalDuration = format(duration);
 
-
   return (
-  
-<CustomBox style={{}}>
-<Layout>
-{/* GRID INTRO */}
+    <>
+<div
+          onMouseMove={handleMouseMove}
+          onMouseLeave={hanldeMouseLeave}
+          ref={playerContainerRef}
+          className={classes.playerWrapper}
+        >
+<div className="wrap-element" style={{position:'relative', top:'0', zIndex:'', height:'80vh', overflow:'hidden'}}>
 
-<div id="" className="wrap-element tronpanel" style={{position:'absolute', top:'0', zIndex:'2', overflow:'', height:'100vh', width:'100vw'}}>
-<h1 className="tronText" style={{display:'grid', placeContent:'center', textAlign:'center', fontSize:'8vw', position:'absolute', top:'0', marginTop:'', width:'100%', padding:'0 30%', opacity:'1', zIndex:'0'}}>
-        <div className="TRON" style={{fontWeight:'normal', fontFamily:'TronType'}}>THE SIRENS</div>
-      </h1>
-
-{/* <header className="wrapper" style={{textAlign:'center', position:'relative', zIndex:'0'}}>
-        <br /> <br />
-
-    
-    <SorryFolks style={{maxWidth:'90vw', maxHeight:'55vh', display:'grid', placeContents:'center'}}/>
-        <h1 className="TRON tronText neonText" style={{fontSize:'180%', lineHeight:'23vh',}}>This attraction is <div style={{color:'red', paddingTop:'2rem', fontSize:'150%', fontFamily:'san serit !important'}}>closed</div><br /> <div style={{marginTop:'0rem'}}>for maintenance.</div></h1>
-        <p className="tronText" style={{fontSize:'150%', padding:'2rem'}}>
-          Please check back soon!
-        </p>
-      </header> */}
-
-      {/* <div style={{display:'flex', justifyContent:'center', width:'100%', marginTop:'2rem', gap:'2rem'}}>
-        <Link to="/" className="actionJackson">
-        <RiArrowLeftSLine className="icon -left" />
-        Back to Homepage
-      </Link>
-      <Link to="/contact" className="actionJackson">
-        Report this <RiBugLine className="icon -right" />
-      </Link></div> */}
-
-   
-
-
-
-{/* Panel Video */}
-{/* <div className="vidbox" style={{position:'absolute', height:'100vh', width:'100vw', bottom:'0', zIndex:''}}>
-<div className="video-background">
-<div className="video-foreground"> */}
-{/* c_V1iD6F1kk */}
-{/* nJ38P5elTkg */}
-{/* <div id="homestart" style={{position:'absolute', top:'-100vh'}}></div> */}
-{/* https://www.youtube.com/embed/yyovIAXi5mc */}
-<div className="wrap-element" style={{overflow:'hidden', position:'relative', top:'0'}}>
-
-
-<ReactPlayer
+        
+          <ReactPlayer
             ref={playerRef}
             width="100%"
             height="100%"
-            url="https://www.youtube.com/embed/yyovIAXi5mc"
+            // url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+            url="https://www.youtube.com/embed/CvyuuTc8F2o"
             pip={pip}
             playing={playing}
             controls={controls}
@@ -414,46 +349,12 @@ function Sirens() {
                 },
               },
               youtube: {
-                playerVars: { showinfo:0, autoplay:1, controls:0, mute:0, start:20, end:120, loop:1  }
+                playerVars: { rel:0, showinfo:0, autoplay:1, controls:0, mute:0, start:20, end:120, loop:1  }
               },
             }}
-            playsinline
-            playIcon={
-              <button aria-label="Click To Play" className="clickplay" style={{position:'', zIndex:'5', bottom:'0', border:'0px solid red', width:'100vw', height:'100vh', background:'', color:'#fff', fontSize:'18px', textAlign:'center', display:'flex', flexDirection:'columh', verticalAlign:'center', justifyContent:'center', alignItem:'center', paddingTop:''}}>
-  
-          <div className="" style={{ textAlign:'center', animation:'fadeIn 3s'}}>
-            
-  
-
-        
-            <span style={{fontWeight:'bold', padding:'0 0 0 0', fontSize:'2rem'}}>Click To Play</span>
-    <ImPlay style={{margin:'0 auto', width:'50%', fontSize:'60px'}} />
-            </div>
-            </button>}
           />
 
-
-{/* <div style={{position:'absolute', bottom:'0', left:'0', zIndex:'1', width:'100vw', height:'100%', border:'0px solid yellow', padding:'0', margin:'0'}}>
-<StaticImage className=""
-alt="Todd Lambert Web development for photographers" src="../../static/assets/shawshank-stuff.png" style={{height:'100vh'}}  />
-</div> */}
-
-
-
-
-
-
-          {/* <Controls
-            ref={controlsRef}
-            onPlayPause={handlePlayPause}
-            playing={playing}
-            played={played}
-            onMute={hanldeMute}
-            muted={muted}
-          /> */}
-
-
-<Controls
+          <Controls
             ref={controlsRef}
             onSeek={handleSeekChange}
             onSeekMouseDown={handleSeekMouseDown}
@@ -477,82 +378,34 @@ alt="Todd Lambert Web development for photographers" src="../../static/assets/sh
             volume={volume}
             // onBookmark={addBookmark}
           />
-      
-     
-{/* Panel Video */}
+        </div>
+        </div>
 
+        {/* <Grid container style={{ marginTop: 20 }} spacing={3}>
+          {bookmarks.map((bookmark, index) => (
+            <Grid key={index} item>
+              <Paper
+                onClick={() => {
+                  playerRef.current.seekTo(bookmark.time);
+                  controlsRef.current.style.visibility = "visible";
 
-
-
-
-
-{/* Panel Content */}
-
-
-
- 
-
-    
-      <div style={{border:'0px red', display:'block', width:'100vw', height:'100vh', position:'absolute', zIndex:'1', bottom:'0', left:'', background:'transparent', display:'grid', placeContent:'center'}}>
-
-
-      <div style={{display:'', justifyContent:'', color:'#ccc', position:'relative', bottom:'-45vh',}}><a href="/proceed-to-games/" title="go back"><button className=" " style={{display:'flex', justifyContent:'center'}}>Go Back</button></a></div>
-      </div>
-
-
-      </div>
-
-
-
-
-
-      </div>
-
-     
-    
-
-
-   
-    
-
-
-
-  
-
-       
-
-
-
-
-{/* Panel Content */}
-
-
-
-
-
-{/*  */}
-{/* Image Gallery */}
-
-  {/* {data.allFile.edges.map(edge => {
-  return <GatsbyImage
-  image={edge.node.childImageSharp.gatsbyImageData}
-  srl_gallery_image="true"
-  alt={edge.node.name}
-  key={edge.node.id}
-/>
-})} */}
-
-
-
-
-
-
-
-
-
-</Layout>
-</CustomBox>
+                  setTimeout(() => {
+                    controlsRef.current.style.visibility = "hidden";
+                  }, 1000);
+                }}
+                elevation={3}
+              >
+                <img crossOrigin="anonymous" src={bookmark.image} />
+                <Typography variant="body2" align="center">
+                  bookmark at {bookmark.display}
+                </Typography>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+        <canvas ref={canvasRef} /> */}
+    </>
   );
 }
 
-export default Sirens;
+export default VideoTest;
