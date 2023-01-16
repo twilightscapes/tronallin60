30ade138b3fe4e3393b5ae0e69338089eb849f00
Todd <ihatespam@gmail.com>
2023-01-16 11:15:43 -0600

    Update Controls.js

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -213,11 +213,11 @@ const Controls = forwardRef(
                 >
 
       {muted ? (
-<MdVolumeDown className="hudicon" fontSize="large" style={{}}  />
-        ) : volume > 0.5 ? (
-<MdVolumeOff className="hudicon" fontSize="large" style={{}}  />
-        ) : (
 <MdVolumeUp className="hudicon" fontSize="large" style={{}}  />
+        ) : volume > 0.5 ? (
+<MdVolumeUp className="hudicon" fontSize="large" style={{}}  />
+        ) : (
+<MdVolumeOff className="hudicon" fontSize="large" style={{}}  />
         )}
 
                 </div>
